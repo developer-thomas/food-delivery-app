357c1a6bf94d37a1ed77fdf945698dea6b1f6c6d
Thomas Edson <andepipous@gmail.com>
2024-08-28 00:27:38 -0400

    feat: create details page and its logics

--- src/app/home/screens/detail/detail.module.ts
+++ src/app/home/screens/detail/detail.module.ts
@@ -7,14 +7,18 @@ import { IonicModule } from '@ionic/angular';
 import { DetailPageRoutingModule } from './detail-routing.module';
 
 import { DetailPage } from './detail.page';
+import { BadgeComponent } from 'src/app/components/badge/badge.component';
+import { ButtonComponent } from 'src/app/components/button/button.component';
+import { ButtonModule } from 'src/app/components/button/button.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    DetailPageRoutingModule
+    DetailPageRoutingModule,
+    ButtonModule,
   ],
-  declarations: [DetailPage]
+  declarations: [DetailPage, BadgeComponent],
 })
 export class DetailPageModule {}
